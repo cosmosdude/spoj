4191b9bc2d6b97b7ec6491291773588f96e46ac5
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2021-10-27 21:28:44 +0630

    XOR (#simple-math #basics)

---
+++ 17921XOR.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <array>
+#include <algorithm>
+#include <cstring>
+#include <numeric>
+#include <iostream>
+#include <list>
+#include <forward_list>
+
+using namespace std;
+
+int main() {
+	int a, b; cin >> a >> b;
+	cout << (a^b) << endl;
+	return 0;
+}
